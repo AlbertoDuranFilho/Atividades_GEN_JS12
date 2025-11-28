428a47ae6fc00d564fe7358e3c77529b59d540c8
Alberto Duran <albertoduranfilho@gmail.com>
2025-11-28 12:01:34 -0300

    :sparkles: feat: Adicionando metodo para o resultado mostrar as casas decimais corretas.

--- Dia5_Atividade_Pratica/mediaParticipante.js
+++ Dia5_Atividade_Pratica/mediaParticipante.js
@@ -8,4 +8,4 @@ let nota4 = prompt.questionFloat("Digite a quarta nota: ");
 let media = (nota1 + nota2 + nota3 + nota4) / 4; 
 
 
-console.log(`A média final foi de: ${media}`)
+console.log(`A média final foi de: ${media.toFixed(1)}`)
