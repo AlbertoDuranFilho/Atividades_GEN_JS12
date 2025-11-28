aed454ff0cc2d03f28643259c973d6e2a29cc2e3
Alberto Duran <albertoduranfilho@gmail.com>
2025-11-28 11:20:05 -0300

    :sparkles: feat: Adicionando primeira atividade.

---
+++ Dia5_Atividade_Pratica/salarioAbono.js
@@ -0,0 +1,9 @@
+let prompt = require('readline-sync');
+
+let salario = prompt.questionFloat("Digite o salario do funcionario: ");
+
+let abono = prompt.questionFloat("Digite o valor do abono: ");
+
+let novoSalario = salario + abono;
+
+console.log(`Seu novo salário é ${novoSalario}`)
